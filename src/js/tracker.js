902d72359aa535e19dac5b4b210d0d0135484c8e
mhadam <michael@hadam.us>
2017-12-11 11:13:25 -0500

    Add identifyUser as alias for setUserId (close #621)

--- src/js/tracker.js
+++ src/js/tracker.js
@@ -1955,6 +1955,15 @@
 			},
 
 			/**
+			 * Alias for setUserId.
+			 *
+			 * @param string userId The business-defined user ID
+			 */
+			identifyUser: function(userId) {
+				setUserId(userId);
+			},
+
+			/**
 			 * Set the business-defined user ID for this user using the location querystring.
 			 *
 			 * @param string queryName Name of a querystring name-value pair
